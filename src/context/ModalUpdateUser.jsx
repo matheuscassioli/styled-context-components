2679aae2632f04b844bd996b94eab3449363490c
Matheus Cassioli <matheuscassioli@hotmail.com>
2024-12-05 22:59:37 -0300

    feature(modal) : renderizando dados do usuario na modal update

--- src/context/ModalUpdateUser.jsx
+++ src/context/ModalUpdateUser.jsx
@@ -5,25 +5,25 @@ export const ModalUpdateUserContext = createContext();
 export const ModalUpdateUserProvider = ({ children }) => {
 
     const [showModalUpdateUser, setShowModalUpdateUser] = useState(false);
-
     const [userForUpdate, setUserForUpdate] = useState({})
 
-    const updateUser = (isTable = true, user) => {
+    const updateUser = (isTable, user) => {
         if (isTable) {
             setShowModalUpdateUser(true)
-            setUserForUpdate(user.id)
+            setUserForUpdate(user)
             return
-        }
-        updateUserApi()
+        } 
+        console.log()
     }
 
-    const updateUserApi = () => {
-        console.warn('chamada para editar usuario', { id: userForUpdate })
+    const updateUserApi = (e, newUser) => {
+        e.preventDefault()
+        console.warn('chamada para editar usuario', newUser)
     }
 
     return (
         <ModalUpdateUserContext.Provider
-            value={{ showModalUpdateUser, setShowModalUpdateUser, updateUser }}>
+            value={{ showModalUpdateUser, setShowModalUpdateUser, updateUser, userForUpdate }}>
             {children}
         </ModalUpdateUserContext.Provider>
     );
